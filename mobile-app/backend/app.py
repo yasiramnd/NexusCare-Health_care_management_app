49d4853b2f888a2c477d3ae7208a50a00f32d4e9
ChamathkaKariyawasam <chamathkaimen@gmail.com>
2026-03-15 12:09:10 +0530

    upload app.py into mobile-app backend

---
+++ mobile-app/backend/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_cors import CORS
+
+from routes.availability import get_available_times
+from routes.appointments import book_appointment
+from routes.medical_records import get_medical_records
+from routes.prescriptions import get_prescriptions
+from routes.lab_reports import get_lab_reports
+from routes.emergency import update_emergency_profile
+from routes.emergency_visibility import update_emergency_visibility
+from routes.order_medecine import order_medicine
+from routes.auth import register_user, login_user
+
+
+app = Flask(__name__)
+CORS(app)
+
+# Register routes
+get_available_times(app)
+book_appointment(app)
+get_medical_records(app)
+get_prescriptions(app)
+get_lab_reports(app)
+update_emergency_profile(app)
+update_emergency_visibility(app)
+order_medicine(app)
+register_user(app)
+login_user(app)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
