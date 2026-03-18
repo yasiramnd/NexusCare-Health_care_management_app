69243ea4a28d618a27e4e6ac33411e7a66a675d0
ChamathkaKariyawasam <chamathkaimen@gmail.com>
2026-03-18 23:02:28 +0530

    create get_requested_lab_reports function inside the labr_reports.py

--- mobile-app/backend/routes/lab_reports.py
+++ mobile-app/backend/routes/lab_reports.py
@@ -39,3 +39,42 @@ def get_lab_reports(app):
 
         except Exception as e:
             return jsonify({"error": str(e)}), 500
+
+
+
+def get_requested_lab_reports(app):
+
+    @app.route("/patient/lab-reports/requested/<patient_id>", methods=["GET"])
+    def requested_lab_reports(patient_id):
+
+        try:
+            conn = get_db()
+            cur = conn.cursor()
+
+            cur.execute("""
+                SELECT
+                    lr.request_id,
+                    lr.test_name,
+                    lr.priority,
+                    ud.name AS doctor_name,
+                    ul.name AS lab_name
+                FROM lab_requests lr
+                JOIN doctor d ON d.doctor_id = lr.doctor_id
+                JOIN users ud ON d.user_id = ud.user_id
+                JOIN lab l ON l.lab_id = lr.lab_id
+                JOIN users ul ON l.user_id = ul.user_id
+                WHERE lr.patient_id = %s;
+            """, (patient_id,))
+
+            reports = cur.fetchall()
+
+            cur.close()
+            conn.close()
+
+            return jsonify({
+                "patient_id": patient_id,
+                "requested_lab_reports": reports
+            })
+
+        except Exception as e:
+            return jsonify({"error": str(e)}), 500
